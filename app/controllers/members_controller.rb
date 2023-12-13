a1530bee5269687ac7e949bffcccb3cd3df69e22
Kevin Cosio <krcs1801@gmail.com>
2023-12-13 15:43:27 -0600

    invite users to be members of tenant

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,6 +4,17 @@ class MembersController < ApplicationController
     @members = @current_tenant.members
   end
 
+  def invite
+    email= params[:email]
+    return redirect_to tenant_members_path(@current_tenant), alert: "Please enter an email address" if email.blank?
+
+    user= User.find_by(email: email) || User.invite!({email:}, current_user)
+    return redirect_to tenant_members_path(@current_tenant), notice: "User #{email} is not valid" unless user.valid?
+
+    user.members.find_or_create_by(tenant: @current_tenant,roles: {admin: false, editor: true})
+    redirect_to tenant_members_path(@current_tenant), notice: "User #{email} was invited to join the team."
+  end
+
   private
 
   def set_current_tenant
