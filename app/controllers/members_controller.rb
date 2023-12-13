e216106dba1cde0d2cf9f3981f35ef882f08b9e9
Kevin Cosio <krcs1801@gmail.com>
2023-12-13 15:30:40 -0600

    display tenant members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,12 @@
 class MembersController < ApplicationController
+  before_action :set_current_tenant
+  def index
+    @members = @current_tenant.members
+  end
+
+  private
+
+  def set_current_tenant
+    @current_tenant = Tenant.find(params[:tenant_id])
+  end
 end
